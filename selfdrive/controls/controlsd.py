af067780042df344205eac3aefc1d5a5bdd8e48c
Jacob Pfeifer <jacob@pfeifer.dev>
2023-06-10 15:35:40 -0400

    gap adjust control

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -29,6 +29,10 @@ from openpilot.selfdrive.controls.lib.alertmanager import AlertManager, set_offr
 from openpilot.selfdrive.controls.lib.vehicle_model import VehicleModel
 from openpilot.system.hardware import HARDWARE
 
+# PFEIFER - GAC {{
+from openpilot.selfdrive.controls.gap_adjust_control import gap_adjust
+# }} PFEIFER - GAC
+
 SOFT_DISABLE_TIME = 3  # seconds
 LDW_MIN_SPEED = 31 * CV.MPH_TO_MS
 LANE_DEPARTURE_THRESHOLD = 0.1
@@ -849,6 +853,10 @@ class Controls:
 
     self.update_events(CS)
     cloudlog.timestamp("Events updated")
+
+    # PFEIFER - GAC {{
+    gap_adjust.update()
+    # }} PFEIFER - GAC
 
     if not self.read_only and self.initialized:
       # Update control state
