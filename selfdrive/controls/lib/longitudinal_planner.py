ae2942d0680fbc3c4b1168e8a297e0a938882450
Jacob Pfeifer <jacob@pfeifer.dev>
2023-06-19 10:23:42 -0400

    Use the cluster speed when limiting speed

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -93,9 +93,13 @@ class LongitudinalPlanner:
     self.mpc.mode = 'blended' if sm['controlsState'].experimentalMode else 'acc'
 
     v_ego = sm['carState'].vEgo
+    v_ego_cluster = sm['carState'].vEgoCluster;
+    v_ego_diff = 0
+    if v_ego_cluster > 0:
+      v_ego_diff = v_ego_cluster - v_ego;
     v_cruise_kph = sm['controlsState'].vCruise
     v_cruise_kph = min(v_cruise_kph, V_CRUISE_MAX)
-    v_cruise = v_cruise_kph * CV.KPH_TO_MS
+    v_cruise = v_cruise_kph * CV.KPH_TO_MS + v_ego_diff
 
     long_control_off = sm['controlsState'].longControlState == LongCtrlState.off
     force_slow_decel = sm['controlsState'].forceDecel
