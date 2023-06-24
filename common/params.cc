2e845f5bc39280be2ad3989c28f0ccacfb975bcc
Jacob Pfeifer <jacob@pfeifer.dev>
2023-06-24 17:59:36 -0400

    gap adjust button

--- common/params.cc
+++ common/params.cc
@@ -207,6 +207,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+
+    // PFEIFER - GAB {{
+    {"GapAdjustButton", PERSISTENT},
+    // }} PFEIFER - GAB
 };
 
 } // namespace
