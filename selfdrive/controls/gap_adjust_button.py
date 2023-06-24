0476f3f6433afd197f7217628b4223ccb9efb6ef
Jacob Pfeifer <jacob@pfeifer.dev>
2023-06-24 17:59:36 -0400

    gap adjust button

---
+++ selfdrive/controls/gap_adjust_button.py
@@ -0,0 +1,8 @@
+# PFEIFER - GAB
+
+from selfdrive.controls.button_manager import Button, ButtonState
+
+# export button state so only one import is necessary
+GapButtonState = ButtonState
+
+gap_adjust_button = Button("GapAdjustButton")
