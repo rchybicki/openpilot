9ef2cc8d7c4029643262a462c7c351733e37296b
RCH <rchybicki@gmail.com>
2023-07-17 09:57:18 +0200

    SLC
    # Conflicts:
    #	selfdrive/controls/speed_limit_controller.py
    #	selfdrive/mapd/mapd.py
    #	selfdrive/ui/qt/onroad.cc

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -143,9 +143,9 @@ class LongitudinalPlanner:
     accel_limits_turns[1] = max(accel_limits_turns[1], self.a_desired - 0.05)
 
     # PFEIFER - SLC {{
-    slc.update_current_max_velocity(v_cruise_kph * CV.KPH_TO_MS)
-    if slc.speed_limit > 0 and (slc.speed_limit + slc.offset + v_ego_diff) < v_cruise:
-      v_cruise = slc.speed_limit + slc.offset + v_ego_diff
+    slc.update_current_max_velocity(v_ego)
+    if slc.speed_limit > 0 and (slc.speed_limit + slc.offset) < v_cruise:
+      v_cruise = slc.speed_limit + slc.offset
     # }} PFEIFER - SLC
     # PFEIFER - VTSC {{
     enabled = not reset_state and self.CP.openpilotLongitudinalControl
