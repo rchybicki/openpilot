066233bc9a90e09078ae19aa55e8ab7503a3834b
RCH <rchybicki@gmail.com>
2023-07-21 21:52:32 +0200

    Babysitter tune

--- selfdrive/navd/navd.py
+++ selfdrive/navd/navd.py
@@ -229,6 +229,7 @@ class RouteEngine:
 
       if slc.speed_limit != 0:
         msg.navInstruction.speedLimit = slc.speed_limit
+        msg.navInstruction.speedLimitSign = log.NavInstruction.SpeedLimitSign.vienna
       # }} PFEIFER - SLC
       put_bool_nonblocking("ExperimentalControl-NavdTurn", False)
       self.pm.send('navInstruction', msg)
@@ -322,6 +323,9 @@ class RouteEngine:
 
     if slc.speed_limit != 0:
       msg.navInstruction.speedLimit = slc.speed_limit
+      msg.navInstruction.speedLimitSign = log.NavInstruction.SpeedLimitSign.vienna
+
+
     # }} PFEIFER - SLC
 
     # Speed limit sign type
