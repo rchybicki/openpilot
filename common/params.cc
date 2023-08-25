b67cf56740aacd6ba0807616b34faa9ef996ad65
Jacob Pfeifer <jacob@pfeifer.dev>
2023-08-24 21:03:13 -0400

    Lane Detection
    Prevents lane changes whenever there is no lane detected.

--- common/params.cc
+++ common/params.cc
@@ -210,6 +210,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+
+    // PFEIFER - LD {{
+    {"LaneDetection", PERSISTENT},
+    // }} PFEIFER - LD
 };
 
 } // namespace
