2536366a8b178216c9d31068e4f5a0ed526c8dd8
Jacob Pfeifer <jacob@pfeifer.dev>
2023-08-24 21:04:13 -0400

    Nudgless Lane Change
    Enables automatically changing lanes when the blinker is turned on.

--- common/params.cc
+++ common/params.cc
@@ -206,6 +206,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+
+    // PFEIFER - NLC {{
+    {"NudgelessLaneChange", PERSISTENT},
+    // }} PFEIFER - NLC
 };
 
 } // namespace
