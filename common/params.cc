ba69bb1bf3b03e4910e175de6b7b3913484faea4
Jacob Pfeifer <jacob@pfeifer.dev>
2023-09-13 20:05:30 -0400

    disable dcam upload

--- common/params.cc
+++ common/params.cc
@@ -206,6 +206,9 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+    // PFEIFER - DDU {{
+    {"DisableDCamUpload", PERSISTENT},
+    // }} PFEIFER - DDU
 };
 
 } // namespace
