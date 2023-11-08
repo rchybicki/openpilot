e74f34c9e002bf3156770e9bdbb4ad85bb075c7c
Jacob Pfeifer <jacob@pfeifer.dev>
2023-11-08 18:33:57 -0500

    update mapd

--- selfdrive/mapd.py
+++ selfdrive/mapd.py
@@ -5,7 +5,7 @@ import urllib.request
 from openpilot.common.realtime import Ratekeeper
 import stat
 
-VERSION = 'v1.5.0'
+VERSION = 'v1.5.1'
 URL = f"https://github.com/pfeiferj/openpilot-mapd/releases/download/{VERSION}/mapd"
 MAPD_PATH = '/data/openpilot/mapd'
 VERSION_PATH = '/data/openpilot/mapd_version'
