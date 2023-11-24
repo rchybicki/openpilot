78a71371781e33316cb4fe6bacc64f264df591bd
Jacob Pfeifer <jacob@pfeifer.dev>
2023-11-23 21:25:32 -0500

    mtsc

--- common/params.cc
+++ common/params.cc
@@ -208,6 +208,11 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+
+    // PFEIFER - MTSC {{
+    {"MapTargetVelocities", PERSISTENT},
+    {"MTSCEnabled", PERSISTENT},
+    // }} PFEIFER - MTSC
 };
 
 } // namespace
