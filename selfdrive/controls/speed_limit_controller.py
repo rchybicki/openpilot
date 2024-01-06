358e2e2b7f0c3ca071bded0c6d88b647ee4191aa
RCH <rchybicki@gmail.com>
2024-01-06 23:21:58 +0100

    slc dict fix

--- selfdrive/controls/speed_limit_controller.py
+++ selfdrive/controls/speed_limit_controller.py
@@ -102,8 +102,8 @@ class SpeedLimitController:
 
       if self.map_next_speed_limit != 0:
         next_way_id_offset = 0
-        if self.map_next_way_id != 0 and self.map_next_way_direction is not None and (self.map_next_way_id, self.map_next_way_direction) in self.overrides:
-          next_way_id_offset = self.overrides[(self.map_way_id, self.map_way_direction)]
+        if self.map_next_way_id != 0 and self.map_next_way_direction is not None and str(self.map_next_way_id)+ str(self.map_next_way_direction) in self.overrides:
+          next_way_id_offset = self.overrides[str(self.map_way_id) + str(self.map_way_direction)]
 
         next_speed_limit_switch_distance = abs(self.map_next_speed_limit + next_way_id_offset - self.vEgo) * self.vEgo \
                   * (0.7 if self.map_next_speed_limit + next_way_id_offset < self.vEgo else 1.5)
@@ -113,14 +113,14 @@ class SpeedLimitController:
 
       if self.last_way_id != self.map_way_id:
           self.way_id_offset = 0
-          if self.map_way_id != 0 and self.map_way_direction is not None and (self.map_way_id, self.map_way_direction) in self.overrides:
-            self.way_id_offset = self.overrides[(self.map_way_id, self.map_way_direction)]
+          if self.map_way_id != 0 and self.map_way_direction is not None and str(self.map_way_id) + str(self.map_way_direction) in self.overrides:
+            self.way_id_offset = self.overrides[str(self.map_way_id) + str(self.map_way_direction)]
           self.last_way_id = self.map_way_id
 
       if self.last_way_direction != self.map_way_direction:
           self.way_id_offset = 0
-          if self.map_way_id != 0 and self.map_way_direction is not None and (self.map_way_id, self.map_way_direction) in self.overrides:
-            self.way_id_offset = self.overrides[(self.map_way_id, self.map_way_direction)]
+          if self.map_way_id != 0 and self.map_way_direction is not None and str(self.map_way_id) + str(self.map_way_direction) in self.overrides:
+            self.way_id_offset = self.overrides[str(self.map_way_id) + str(self.map_way_direction)]
           self.last_way_direction = self.map_way_direction
 
 
@@ -149,7 +149,7 @@ class SpeedLimitController:
       elif lfa_button.simple_state == LFAButtonState.LONG_PRESS and self._offset != 0 and self.speed_limit > 0 and self.map_way_id != 0 and self.map_way_direction is not None:
         self.way_id_offset = self._offset
         self._offset = 0
-        self.overrides[(self.map_way_id, self.map_way_direction)] = self.way_id_offset
+        self.overrides[str(self.map_way_id) + str(self.map_way_direction)] = self.way_id_offset
         self.write_overrides()
         
 
