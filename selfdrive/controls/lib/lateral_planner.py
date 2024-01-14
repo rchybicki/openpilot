fcf7ec031a867b374279c305b14815b23e5b963d
Jacob Pfeifer <jacob@pfeifer.dev>
2024-01-14 16:14:29 -0500

    more dlp fixes

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -12,14 +12,14 @@ from openpilot.selfdrive.controls.lib.lane_planner import LanePlanner
 from openpilot.common.realtime import DT_MDL
 from openpilot.common.numpy_fast import interp
 PATH_COST = 1.0
-LATERAL_MOTION_COST = 0.11
+LATERAL_MOTION_COST = 0.8
 LATERAL_ACCEL_COST = 0.0
-LATERAL_JERK_COST = 0.04
+LATERAL_JERK_COST = 0.02
 # Extreme steering rate is unpleasant, even
 # when it does not cause bad jerk.
 # TODO this cost should be lowered when low
 # speed lateral control is stable on all cars
-STEERING_RATE_COST = 700.0
+STEERING_RATE_COST = 50.0
 # }} PFEIFER - DLP
 
 TRAJECTORY_SIZE = 33
@@ -125,6 +125,7 @@ class LateralPlanner:
         self.solution_invalid_cnt += 1
       else:
         self.solution_invalid_cnt = 0
+      self.x_sol = self.lat_mpc.x_sol
     # }} PFEIFER - DLP
 
   def publish(self, sm, pm):
