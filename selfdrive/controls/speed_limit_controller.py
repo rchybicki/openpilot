f204aa11f9c083723e0e823b9438701592ab4af3
RCH <rchybicki@gmail.com>
2024-01-19 01:02:01 +0100

    slc fix

--- selfdrive/controls/speed_limit_controller.py
+++ selfdrive/controls/speed_limit_controller.py
@@ -197,7 +197,7 @@ class SpeedLimitController:
         if enabled and set_speed - vEgo < limit_diff:
           self.way_id_offset -= offset_tick
         else:
-          self.way_id_offset = ((vEgo - set_speed - self.way_id_offset) // 5) * 5
+          self.way_id_offset = ((vEgo - set_speed + self.way_id_offset) // 5) * 5
         self.set_override(current_way_id, current_way_direction, current_distance_to_end_of_way, vEgo, self.way_id_offset)
       elif lfa_button.simple_state == LFAButtonState.LONG_PRESS and self.speed_limit > 0 and current_way_id != 0 \
               and current_distance_to_end_of_way != 0 and current_way_direction is not None:
