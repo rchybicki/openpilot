c038c322ea4e4738ebb2c687287ba7d7d53f94dc
Radek Chybicki <rchybicki@gmail.com>
2024-08-19 13:49:27 +0200

    show stopping
    # Conflicts:
    #	selfdrive/ui/qt/onroad/annotated_camera.cc
    #	selfdrive/ui/qt/onroad/annotated_camera.h
    #	selfdrive/ui/ui.cc

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -66,7 +66,7 @@ public:
 
 private:
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255, bool overridePen = false);
-  void drawRedText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawTextColor(QPainter &p, int x, int y, const QString &text, const QColor &color);
 
   QVBoxLayout *main_layout;
   ExperimentalButton *experimental_btn;
@@ -85,6 +85,7 @@ private:
   bool has_eu_speed_limit = false;
   bool v_ego_cluster_seen = false;
   bool brake_lights = false;
+  bool stopping = false;
   int status = STATUS_DISENGAGED;
   std::unique_ptr<PubMaster> pm;
 
