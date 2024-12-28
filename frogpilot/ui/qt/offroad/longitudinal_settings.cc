19947572fa5bd5b744cb81beac843c2053c85514
Radek Chybicki <rchybicki@gmail.com>
2024-12-28 11:43:26 +0100

    gap and stop dist
    # Conflicts:
    #	selfdrive/frogpilot/ui/qt/offroad/longitudinal_settings.cc
    
    # Conflicts:
    #	selfdrive/frogpilot/ui/qt/offroad/longitudinal_settings.cc
    
    # Conflicts:
    #	frogpilot/controls/lib/frogpilot_following.py
    
    # Conflicts:
    #	selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py

--- frogpilot/ui/qt/offroad/longitudinal_settings.cc
+++ frogpilot/ui/qt/offroad/longitudinal_settings.cc
@@ -313,7 +313,7 @@ FrogPilotLongitudinalPanel::FrogPilotLongitudinalPanel(FrogPilotSettingsWindow *
         if (param == "TrafficFollow") {
           longitudinalToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.5, 5, QString(), followTimeLabels, 0.01, true);
         } else {
-          longitudinalToggle = new FrogPilotParamValueControl(param, title, desc, icon, 1, 5, QString(), followTimeLabels, 0.01, true);
+          longitudinalToggle = new FrogPilotParamValueControl(param, title, desc, icon, 0.5, 5, QString(), followTimeLabels, 0.01, true);
         }
       } else {
         longitudinalToggle = new FrogPilotParamValueControl(param, title, desc, icon, 25, 200, "%");
