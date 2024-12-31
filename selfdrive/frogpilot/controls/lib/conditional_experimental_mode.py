e70d9913286f31cc9e71a650d5fe5e522939a195
Radek Chybicki <rchybicki@gmail.com>
2024-12-31 10:58:50 +0100

    expc for mtsc curve

--- selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
+++ selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
@@ -87,6 +87,10 @@ class ConditionalExperimentalMode:
       self.status_value = 18
       return True
 
+    if self.frogpilot_planner.frogpilot_vcruise.mtsc_active and v_ego_kph < 120.:
+      self.status_value = 19
+      return True
+
     return False
 
   def update_conditions(self, frogpilotCarState, modelData, tracking_lead, v_ego, v_ego_kph, v_lead, dRel_lead, aLeadK, frogpilot_toggles, personality):
