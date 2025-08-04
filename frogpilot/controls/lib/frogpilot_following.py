d4aa0667d6baca554648b615336a3744b3cdf21a
Radek Chybicki <rchybicki@gmail.com>
2025-08-04 11:11:26 +0200

    distance factor settings
    # Conflicts:
    #	selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
    #	selfdrive/controls/lib/longitudinal_planner.py
    
    # Conflicts:
    #	frogpilot/ui/qt/offroad/longitudinal_settings.h
    
    # Conflicts:
    #	frogpilot/ui/qt/offroad/longitudinal_settings.cc
    
    # Conflicts:
    #	frogpilot/ui/qt/offroad/longitudinal_settings.h
    #	selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
    #	selfdrive/controls/lib/longitudinal_planner.py
    
    # Conflicts:
    #	common/params.cc
    #	frogpilot/common/frogpilot_variables.py
    #	frogpilot/ui/qt/offroad/longitudinal_settings.cc
    #	frogpilot/ui/qt/offroad/longitudinal_settings.h
    
    # Conflicts:
    #	selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
    #	selfdrive/controls/lib/longitudinal_planner.py
    
    # Conflicts:
    #	frogpilot/controls/lib/frogpilot_following.py

--- frogpilot/controls/lib/frogpilot_following.py
+++ frogpilot/controls/lib/frogpilot_following.py
@@ -74,7 +74,16 @@ class FrogPilotFollowing:
     if sm["controlsState"].enabled and self.frogpilot_planner.tracking_lead:
 	  if not sm["frogpilotCarState"].trafficModeEnabled:
         self.update_follow_values(self.frogpilot_planner.lead_one.dRel, v_ego, self.frogpilot_planner.lead_one.vLead, frogpilot_toggles)
-      self.desired_follow_distance = int(desired_follow_distance(v_ego, self.frogpilot_planner.lead_one.vLead, self.frogpilot_planner.lead_one.dRel, self.t_follow))
+      self.desired_follow_distance = int(
+        desired_follow_distance(
+          v_ego,
+          self.frogpilot_planner.lead_one.vLead,
+          self.frogpilot_planner.lead_one.dRel,
+          self.t_follow,
+          frogpilot_toggles.short_distance_factor,
+          frogpilot_toggles.long_distance_factor
+        )
+      )
     else:
       self.desired_follow_distance = 0
 
