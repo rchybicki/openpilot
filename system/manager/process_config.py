7867a24831371ac50ac88201754ca4fe739dcd56
Radek Chybicki <radek@selectviewdata.com>
2026-03-22 14:36:03 +0100

    system: add sshreach and connectivity management

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -62,6 +62,8 @@ def run_tinygrad_modeld(started, params, CP: car.CarParams, classic_model, tinyg
 
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
+  DaemonProcess("manage_sshreachd", "system.sshreach.manage_sshreachd", "SshreachdPid"),
+  DaemonProcess("manage_tailscaled", "system.tailscale.manage_tailscaled", "TailscaledPid"),
 
   NativeProcess("camerad", "system/camerad", ["./camerad"], driverview),
   NativeProcess("logcatd", "system/logcatd", ["./logcatd"], allow_logging),
