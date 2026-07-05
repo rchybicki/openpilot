aee540c0566a4af9fb698010ae3079de3367a8b6
Radek Chybicki <rchybicki@selectviewdata.com>
2026-07-05 12:32:24 +0200

    Limit Hyundai HUD preview to long press

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -213,7 +213,7 @@ class Controls:
     hudControl.leadVisible = self.sm['longitudinalPlan'].hasLead
     personality = self.sm['selfdriveState'].personality.raw
     if self.CP.brand == "hyundai" and self.CP.openpilotLongitudinalControl and self.frogpilot_toggles.personality_profile_via_distance_long and \
-       self.sm['frogpilotCarState'].distancePressed:
+       self.sm['frogpilotCarState'].distanceLongPressed:
       personality = (personality - 1) % 3
     hudControl.leadDistanceBars = personality + 1
     hudControl.visualAlert = self.sm['selfdriveState'].alertHudVisual
